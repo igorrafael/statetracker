045048682ff950b82eba98f29769aba17d41182c
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-09-16 04:27:50 -0400

    Stub trackables

---
+++ script/ITrackable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace StateTracker
+{
+    public interface ITrackable<T>
+    {
+        T[] States
+        {
+            get;
+        }
+        Dictionary<T, T[]> Transitions
+        {
+            get;
+        }
+    }
+}
---
+++ script/TrackableBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateTracker
+{
+    public class TrackableBehaviour<T> : MonoBehaviour, ITrackable<T>
+        where T : struct, IConvertible
+    {
+        public T[] States
+        {
+            get
+            {
+                Array values = Enum.GetValues(typeof(T));
+                return values as T[];
+            }
+        }
+
+        public Dictionary<T, T[]> Transitions
+        {
+            get
+            {
+                return _transitions;
+            }
+        }
+
+        private Dictionary<T, T[]> _transitions = new Dictionary<T, T[]>();
+    }
+}
