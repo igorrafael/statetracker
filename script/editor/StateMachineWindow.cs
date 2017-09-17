eb9c4909484e6f6b27317bd25866572bfd204b34
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-09-16 22:14:47 -0400

    StateMachineWindow stub

---
+++ script/editor/StateMachineWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace StateTracker {
+    class StateMachineWindow : EditorWindow
+    {
+        StateMachine target;
+
+        [MenuItem("StateTracker/State Machine View")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(StateMachineWindow));
+        }
+
+        void OnGUI()
+        {
+            target = EditorGUILayout.ObjectField(target, typeof(StateMachine), true) as StateMachine;
+            if (!target)
+            {
+                return;
+            }
+            foreach (var state in target.States)
+            {
+                EditorGUILayout.LabelField(state.name);
+            }
+        }
+    }
+}
