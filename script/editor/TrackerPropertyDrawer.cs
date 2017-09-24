600837ba395a7a00cf618aa3cde65061d28371d8
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-09-24 16:59:16 -0400

    PropertyDrawer experiments

--- script/editor/TrackerPropertyDrawer.cs
+++ script/editor/TrackerPropertyDrawer.cs
@@ -2,6 +2,7 @@
 using UnityEditor;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace StateTracker.StateMachine
 {
@@ -9,7 +10,9 @@ namespace StateTracker.StateMachine
     public class TrackerDrawer : PropertyDrawer
     {
         float lineHeight = 20f;
-        private bool _showButtons;
+        float buttonWidth = 60f;
+        private Vector2 _buttonScrollPosition;
+        private List<State> _states;
 
         // Draw the property inside the given rect
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
@@ -34,30 +37,55 @@ namespace StateTracker.StateMachine
             //tracker.test = EditorGUI.TextField(position, tracker.test);
             property.objectReferenceValue = tracker;
 
-            Rect buttonPosition = position;
-            buttonPosition.height = lineHeight;
-            _showButtons = EditorGUI.Foldout(buttonPosition, _showButtons, "Missing states");
-            if (_showButtons)
+            string[] values = GetMissingValues(property);
+            Rect scrollRect = position;
+            scrollRect.height = lineHeight * 2;
+            var viewRect = scrollRect;
+            viewRect.position -= _buttonScrollPosition;
+            viewRect.width = buttonWidth * values.Length;
+            _buttonScrollPosition = GUI.BeginScrollView(scrollRect, _buttonScrollPosition, viewRect, true, false);
             {
-                Array values = GetMissingValues(property);
-                foreach (var value in values)
+                var buttonPosition = scrollRect;
+                buttonPosition.position -= _buttonScrollPosition;
+                buttonPosition.height = lineHeight;
+                buttonPosition.width = buttonWidth;
                 {
-                    if (GUI.Button(buttonPosition, "Add " + value))
+                    foreach (var value in values)
                     {
-                        var state = new State()
+                        if (GUI.Button(buttonPosition, "+" + value))
                         {
-                            name = value.ToString()
-                        };
-                        tracker.states.Add(state);
+                            var state = new State()
+                            {
+                                name = value.ToString()
+                            };
+                            tracker.states.Add(state);
+                        }
+                        buttonPosition.x += buttonPosition.width;
                     }
-                    buttonPosition.y += buttonPosition.height;
+                }
+            }
+            GUI.EndScrollView();
+            scrollRect.position += new Vector2(0, scrollRect.height);
+
+            var labelRect = scrollRect;
+            labelRect.height = lineHeight;
+            foreach (var state in _states)
+            {
+                GUI.Label(labelRect, state.name);
+                labelRect.position += new Vector2(0, labelRect.height);
+                foreach (var t in state.transitions)
+                {
+                    //TODO
                 }
             }
 
-            // Set indent back to what it was
-            //EditorGUI.indentLevel = indent;
-
             EditorGUI.EndProperty();
+        }
+
+        private void UpdateStates(SerializedProperty property)
+        {
+            var tracker = property.objectReferenceValue as Tracker;
+            _states = tracker.states;
         }
 
         private string[] GetMissingValues(SerializedProperty property)
@@ -65,15 +93,23 @@ namespace StateTracker.StateMachine
             var trackabletype = fieldInfo.DeclaringType.GetGenericArguments()[0];
             var values = Enum.GetNames(trackabletype);
             var tracker = property.objectReferenceValue as Tracker;
-            return values.Where(v=>tracker.states.All(s=>s.name!=v)).ToArray();
+            return values.Where(v => tracker.states.All(s => s.name != v)).ToArray();
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            float buttons = _showButtons ? lineHeight : GetMissingValues(property).Length * lineHeight;
+            //Scroll rect with "add state"
+            float height = lineHeight * 2f;
 
+            UpdateStates(property);
+            //space for each state
+            foreach (var state in _states)
+            {
+                height += lineHeight;
+            }
+            Debug.Log(height);
 
-            return buttons;
+            return height;
         }
     }
 }
